a2687824354c1bcd4b75419033dcf7cdd42290d4
Hazim-HF <hazim.fitri.hf@gmail.com>
2025-05-24 20:07:01 +0800

    update

--- Project/Proj.py
+++ Project/Proj.py
@@ -1,5 +1,7 @@
 from pyhive import hive
 import pandas as pd
+import os
+os.getcwd()
 
 conn = hive.Connection(
     host="localhost",
@@ -8,6 +10,8 @@ conn = hive.Connection(
 )
 
 cursor = conn.cursor()
+
+
 cursor.execute("SHOW TABLES")
 for result in cursor.fetchall():
     print(result)
@@ -40,3 +44,34 @@ SELECT * FROM project.adult
 
 df = pd.read_sql(query, conn)
 print(df.head())
+
+# python code
+
+adult = pd.read_csv('project/adult.csv', header=None)
+adult.columns = ['age', 'workclass', 'final_weight', 'education', 'education_num', 'marital_status',
+                 'occupation', 'relationship', 'race', 'sex', 'capital_gain', 'capital_loss',
+                 'hours_per_week', 'native_country', 'income']
+adult['age'].value_counts()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS project.my_table (
+               number INT,
+               name STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+STORED AS TEXTFILE
+               """)
+
+cursor.execute("""
+               LOAD DATA INPATH '/home/maria_dev/test.csv' INTO TABLE project.my_table
+               """)
+
+name = pd.read_csv('project/data/imdb/name.basics.tsv/name.basics.tsv', sep='\t')
+name.head()
+
+# check missing value
+name.isna().sum().sum() # 9 missing values.
+
+# check duplicated value
+name.duplicated().sum() # no duplicated values
